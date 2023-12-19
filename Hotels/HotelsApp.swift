15235c8f3d2e4a5d5ba07f8837c2463533c1aadf
Rashid <rashidgaitov@gmail.com>
2023-12-19 13:20:57 +0300

    Initial Commit

---
+++ Hotels/HotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelsApp.swift
+//  Hotels
+//
+//  Created by Rashid Gaitov on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
