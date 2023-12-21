5fe85b1ca123b18485ffd7b19122cedf5a838966
Rashid <rashidgaitov@gmail.com>
2023-12-21 10:36:48 +0300

    UI design fixes. Cleaning code

---
+++ Hotels/Components/DetailButtonView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailButtonView.swift
+//  Hotels
+//
+//  Created by Rashid Gaitov on 21.12.2023.
+//
+
+import Foundation
---
+++ Hotels/Model/Hotel.swift
@@ -0,0 +1,8 @@
+//
+//  Hotel.swift
+//  Hotels
+//
+//  Created by Rashid Gaitov on 21.12.2023.
+//
+
+import Foundation
