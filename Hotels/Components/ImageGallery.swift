96b2c96dc9a7c052ebcef6495a9da49964d66eba
Rashid <rashidgaitov@gmail.com>
2023-12-22 19:41:18 +0300

    finished main screen

---
+++ Hotels/Components/ImageGallery.swift
@@ -0,0 +1,18 @@
+//
+//  ImageGallery.swift
+//  Hotels
+//
+//  Created by Rashid Gaitov on 21.12.2023.
+//
+
+import SwiftUI
+
+struct ImageGallery: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageGallery()
+}
---
+++ Hotels/ViewModel/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  Hotels
+//
+//  Created by Rashid Gaitov on 21.12.2023.
+//
+
+import Foundation
